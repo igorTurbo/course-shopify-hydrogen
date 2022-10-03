0b8ca30fb1bc82610232630c7c2f2f84fcf25f9a
Igor Nascimento Costa <igor.costa@turbopartners.com.br>
2022-10-03 12:40:43 -0300

    Changed shopify config

--- 01-Fundamentals/shopify.config.js
+++ 01-Fundamentals/shopify.config.js
@@ -1,5 +1,7 @@
 export default {
-  storeDomain: 'hydrogen-preview.myshopify.com',
-  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  // storeDomain: 'lojaderoupatest.myshopify.com/',
+  // storefrontToken: '571073b70161d37428f45d6adc5ab36b',
+  storeDomain: 'hydrogen-course.myshopify.com',
+  storefrontToken: '9348b58adf91a9552b9b88247b89fc63',
   storefrontApiVersion: '2022-04',
 };
